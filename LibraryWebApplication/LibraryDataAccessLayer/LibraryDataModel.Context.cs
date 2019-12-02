1551369a5809adaeb5160feae1a1b796317dca98
shettysupreetha <supreethashetty8@gmail.com>
2019-12-02 17:37:16 +0530

    Split model class in to separate project

--- LibraryWebApplication/LibraryDataAccessLayer/LibraryDataModel.Context.cs
+++ LibraryWebApplication/LibraryDataAccessLayer/LibraryDataModel.Context.cs
@@ -9,6 +9,7 @@
 
 namespace LibraryDataAccessLayer
 {
+    using LibraryDataModel.Entity;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
--- LibraryWebApplication/LibraryDataAccessLayer/UnitOfWork.cs
+++ LibraryWebApplication/LibraryDataAccessLayer/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryDataModel.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
